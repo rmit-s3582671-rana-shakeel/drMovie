fc0f4511a747fbb10cdabf9c1fc05ec7dd1b4b5a
Rana Ahmed <ranaahmed@Ranas-iMac.local>
2018-01-16 04:04:10 -0800

    Initial Commit

---
+++ drMovie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  drMovie
+//
+//  Created by Rana Ahmed on 1/16/18.
+//  Copyright © 2018 Rana Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
